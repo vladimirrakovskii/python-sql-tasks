3922e6a7d3ab7c81778b289ee7cae633cf1fe3a0
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:44:37 +0300

    Update solution.py

--- 3/src/solution.py
+++ 3/src/solution.py
@@ -5,5 +5,15 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def batch_insert(connection, products_list):
+    with connection.cursor() as curs:
+        product_data = [(product['name'], product['price'], product['quantity']) for product in products_list]
+        execute_values(curs, "INSERT INTO products (name, price, quantity) VALUES %s", product_data)
 
+def get_all_products(connection):
+    sql = "SELECT * from products ORDER BY price DESC;"
+    with connection.cursor() as curs:
+        curs.execute(sql)
+        rows = curs.fetchall()
+    return rows
 # END
