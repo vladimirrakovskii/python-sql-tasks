236bc81eea9eca393889216330497750a451d44b
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:42:41 +0300

    Update solution.py

--- 1/src/solution.py
+++ 1/src/solution.py
@@ -4,5 +4,22 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def add_movies(connection):
+    sql = "INSERT INTO movies (title, release_year, duration) VALUES ('Godfather', 1972, 175);"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    cursor.close()
 
+    sql2 = "INSERT INTO movies (title, release_year, duration) VALUES ('The Green Mile', 1999, 189);"
+    cursor = connection.cursor()
+    cursor.execute(sql2)
+    cursor.close()
+
+def get_all_movies(connection):
+    sql = "SELECT * from movies;"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
 # END
